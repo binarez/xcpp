743c69a5de0bb8fd3371f5ec5fda7f9281108474
Stéphane Duguay <s@binarez.com>
2019-03-26 10:48:52 -0400

    i* fix, for_r

--- xcpp.sh
+++ xcpp.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 #------------------------------------------------------------------------------
 set -e								# Stop execution on error
-xcppVersion=1
+xcppVersion=0
+xcppVersionRev=1
 #------------------------------------------------------------------------------
 xcppGccUserOptions=""
 xcppExecutionArgIndex=1
-# Eventually, replace -w with -Wno-pragma-once-outside-header : https://gcc.gnu.org/bugzilla/show_bug.cgi?id=89808
-readonly xcppGccHardcodedOptions="-pipe -w -x c++"
+# Eventually add -Wno-pragma-once-outside-header https://gcc.gnu.org/bugzilla/show_bug.cgi?id=89808
+readonly xcppGccHardcodedOptions="-pipe -x c++"
 ProcessXcppGccArgs () {
 	i=1;
 	for arg in "$@"
@@ -29,14 +30,19 @@ echo "
 #include <bits/stdc++.h>
 using namespace std;
 
+#define for_i(FORI_TYPE, FORI_FROM, FORI_TO) \
+                                      for(FORI_TYPE i{FORI_FROM}; \
+                                      ((FORI_FROM) < (FORI_TO)) ? (i < (FORI_TO)) : (i > (FORI_TO)); \
+                                      ((FORI_FROM) < (FORI_TO)) ? ++i : --i )
+
 using strings = vector< string >;
 using size = size_t;
 using ssize = ssize_t;
 
-using i8 = uint8_t;
-using i16 = uint16_t;
-using i32 = uint32_t;
-using i64 = uint64_t;
+using i8 = int8_t;
+using i16 = int16_t;
+using i32 = int32_t;
+using i64 = int64_t;
 
 using u8 = uint8_t;
 using u16 = uint16_t;
@@ -134,14 +140,7 @@ inline bool read( T & val )
 
 inline bool readln( string & val )
 {
-	if( getline( cin, val ) )
-	{
-		return true;
-	}
-	else
-	{
-		return false;
-	}
+	return static_cast< bool >( getline( cin, val ) );
 }
 
 inline void press_enter()
@@ -167,7 +166,7 @@ inline void seed_rand( unsigned int seed = 0 )
 }
 #------------------------------------------------------------------------------
 if [[ $# -lt 1 ]]; then				# We need at last one command argument: the source file
-    echo "xcpp.sh version $xcppVersion"
+    echo "xcpp.sh version $xcppVersion.$xcppVersionRev"
 	echo ""
 	echo "Usage:"
 	echo "$0 FILE.xcpp [ARG1 ARG2 ... ARGN]"
@@ -180,21 +179,23 @@ if [[ $# -lt 1 ]]; then				# We need at last one command argument: the source fi
 	exit 42
 fi
 ProcessXcppGccArgs "$@"
+trap "{ rm -f $tempElfFile; rm -f $tempIncludeFile; }" EXIT # Trap exit for temp files removal
 tempElfFile=$(mktemp /tmp/xcpp.XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX.elf)
 tempIncludeFile=$(mktemp /tmp/xcpp.XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX.h)
-trap "{ rm -f $tempElfFile; rm -f $tempIncludeFile; }" EXIT # Trap exit for temp files removal
 GenerateTempXcppHeader $tempIncludeFile
 funcname=$(basename "${!xcppExecutionArgIndex}")
 funcname=${funcname%%.*}
 echo "
 #include <vector>
 #include <string>
+//#include <clocale>
 int main(int _xcpp_reserved_argc_, const char * _xcpp_reserved_argv_[])
 {
+	//setlocale(LC_ALL, "");
 	int $funcname( std::vector<std::string> );
 	return $funcname( std::vector<std::string>(_xcpp_reserved_argv_ + 1, _xcpp_reserved_argv_ + _xcpp_reserved_argc_) );
 }
-" | g++ $xcppGccHardcodedOptions -include $tempIncludeFile -DXCPP_VERSION=$xcppVersion $xcppGccUserOptions -o "$tempElfFile" /dev/stdin "${!xcppExecutionArgIndex}";	# Compile
+" | g++ $xcppGccHardcodedOptions -include $tempIncludeFile -D__XCPP__VERSION__=$xcppVersion $xcppGccUserOptions -o "$tempElfFile" /dev/stdin "${!xcppExecutionArgIndex}"
 chmod +x "$tempElfFile";	# Make executable
 "$tempElfFile" "${@:1}";	# Execute
 e="$?";					# Capture exit code
